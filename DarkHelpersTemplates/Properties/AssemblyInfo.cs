eca55b9b2e2819a9023c9bf245894b98eb87e115
Michal Diviš <A408674@dekra.org>
2021-02-01 15:48:25 +0100

    *

--- DarkHelpersTemplates/Properties/AssemblyInfo.cs
+++ DarkHelpersTemplates/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DarkHelpers.WPF.View")]
+[assembly: AssemblyTitle("DarkHelpersTemplates")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Michal Diviš")]
-[assembly: AssemblyProduct("DarkHelpers.WPF.View")]
-[assembly: AssemblyCopyright("Copyright © Michal Diviš 2021")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DarkHelpersTemplates")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -18,9 +18,6 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d4526541-e8de-4043-9599-e8877eaf0d80")]
 
 // Version information for an assembly consists of the following four values:
 //
