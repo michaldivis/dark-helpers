ced79c6613310d97c4e7648718d6ef80012580db
Michal Diviš <michall.divis@gmail.com>
2022-01-05 16:12:51 +0100

    BaseViewModel tweaks

--- src/DarkHelpers/DarkViewModel.cs
+++ src/DarkHelpers/DarkViewModel.cs
@@ -66,23 +66,23 @@ namespace DarkHelpers
 		/// <summary>
 		/// One time initialzation method, to be called when the view model is first used
 		/// </summary>
-		public virtual async Task OnInitializeAsync() { }
+		public virtual Task OnInitializeAsync() => Task.CompletedTask;
 
 		/// <summary>
 		/// To be called whenever the view model is brought to user's attention, i.e. anytime the corresponding view is shown
 		/// </summary>
-		public virtual async Task OnRefreshAsync() { }
+		public virtual Task OnRefreshAsync() => Task.CompletedTask;
 
 		/// <summary>
 		/// To be called before a view is exited, determines whether the exit should be continued or cancelled
 		/// </summary>
 		/// <returns></returns>
-		public virtual async Task<bool> OnBeforeExitAsync() { return false; }
+		public virtual Task<bool> OnBeforeExitAsync() => Task.FromResult(false);
 
 		/// <summary>
 		/// To be called when a view is exiting, useful for any cleanup work
 		/// </summary>
 		/// <returns><c>true</c> is the exit should be cancelled; otherwise, <c>false</c></returns>
-		public virtual async Task OnExitAsync() { }
+		public virtual Task OnExitAsync() => Task.CompletedTask;
 	}
 }
