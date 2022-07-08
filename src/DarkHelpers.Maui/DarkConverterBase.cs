bc8432fd7f0a3941829711408160997f77d547be
Michal Diviš <michall.divis@gmail.com>
2022-07-08 12:49:23 +0200

    added Maui specific implementations

---
+++ src/DarkHelpers.Maui/DarkConverterBase.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+
+namespace DarkHelpers.Maui;
+
+public abstract class DarkConverterBase : IValueConverter
+{
+    public virtual object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException($"The {nameof(Convert)} method is not supported in {GetType().Name}.");
+    }
+
+    public virtual object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException($"The {nameof(ConvertBack)} method is not supported in {GetType().Name}.");
+    }
+}
